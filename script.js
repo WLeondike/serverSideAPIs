10d51c98d647166e61a0fc100ebe4c67b2a75082
WLeondike <billyleondike@gmail.com>
2020-06-23 21:48:18 -0400

    added if else

--- script.js
+++ script.js
@@ -3,28 +3,47 @@ const city = $(".city");
 const wind = $(".wind");
 const temp = $(".temp");
 const humidity = $(".humidity");
-const searchHistory = [];           //users searches are stored here
+
+//users searches are stored here
+const searchHistory = [];           
 
 $(document).ready(function(){
 
-const queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=596ff6a8b9059e68a3a5b2f8821c0145";
+    usersSearches();
 
+    $("#userSearch").click(function (event){
+        event.preventDefault();
+        let usrSearch = $("#usrInput").val().trim;
+        searchInput(usrSearch);
+    });
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function (response) {
-    console.log(queryURL);
-    console.log(response);
+    function searchInput(cityName);
 
-    // Log the data in HTML
-    $(".city").html("<h1>" + response.name + " Weather Details</h1>");
-    $(".wind").text("Wind Speed " + response.wind.speed);
-    $(".humidity").text("humidity: " + response.main.humidity);
+    const queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=596ff6a8b9059e68a3a5b2f8821c0145";
+    $("<button>").text(cityName).prepend(".list-group-item");
 
-    //celcius to fahrenheit
-    const tempF = (response.main.temp - 273.15) * 1.80 + 32;
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        let lastCityInput = JSON.parse(localStorage.getItem("cities"));
+        if (lastCityInput) {
+            lastCityInput.push(response.name)
+            localStorage.setItem("cities", JSON.stringify(lastCityInput));
+        }else {
+            searchHistory.push(response.name);
+            localStorage.setItem("cities", JSON.stringify(searchHistory));
+        }
+
+        // Log the data in HTML
+        
+
+        //celcius to fahrenheit
+        const tempF = (response.main.temp - 273.15) * 1.80 + 32;
+            
+        }
+    });
 
 });
 
-});
+
